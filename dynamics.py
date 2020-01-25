9746e2f7eafc7ee00b49ffae2ae8cb2298aa918f
xiangyu_liu <liuxiangyu999@sjtu.edu.cn>
2020-01-25 12:07:34 -0800

    debug

--- dynamics.py
+++ dynamics.py
@@ -1,7 +1,9 @@
 import numpy as np
 # np.random.seed(0)
 import warnings
-import control
+import scipy
+import scipy.linalg
+
 warnings.simplefilter('error', RuntimeWarning)
 
 
@@ -16,12 +18,14 @@ class Dynamics:
         self.B = np.random.rand(*[self.state_dim, self.action_dim])
 
         A_eig_max = max([i.real for i in np.linalg.eigvals(self.A) if i.imag == 0])
-        print(A_eig_max)
+        # print(A_eig_max)
         if A_eig_max >= 1:
-            self.A = self.A - (A_eig_max - 0.5) * np.diag([1, ] * self.state_dim)
-            # self.A = self.A / A_eig_max
-        print("max value:", max([i.real for i in np.linalg.eigvals(self.A) if i.imag == 0]))
+            # self.A = self.A - (A_eig_max - 0.5) * np.diag([1, ] * self.state_dim)
+            self.A = self.A / (A_eig_max)
+        # print("max value:", max([i.real for i in np.linalg.eigvals(self.A) if i.imag == 0]))
+
         self.Q = np.random.rand(*[self.state_dim, self.state_dim])
+        self.Q = (self.Q + self.Q.T) / 2
         Q_eig_min = min(i.real for i in np.linalg.eigvals(self.Q) if i.imag == 0)
         # print(Q_eig_min)
         # make sure Q is definite
@@ -29,6 +33,7 @@ class Dynamics:
             self.Q = self.Q - Q_eig_min * np.diag([1, ] * self.state_dim)
 
         self.R = np.random.rand(*[self.action_dim, self.action_dim])
+        self.R = (self.R + self.R.T) / 2
         R_eig_min = min(i.real for i in np.linalg.eigvals(self.R) if i.imag == 0)
         # print(R_eig_min)
         # make sure R is definite
@@ -56,6 +61,9 @@ class Dynamics:
             [np.linalg.inv(self.R + np.linalg.multi_dot([self.B.T, P, self.B])), self.B.T, P, self.A])
 
     def cal_approx_P(self):
+        return scipy.linalg.solve_discrete_are(self.A, self.B, self.Q, self.R)
+
+    def cal_approx_P_manually(self):
         P_old = self.Q
         i = 0
         while True:
@@ -72,5 +80,3 @@ class Dynamics:
                 break
         return P_new
 
-    def test_control(self):
-        pass
--- lqr.py
+++ lqr.py
@@ -9,6 +9,7 @@ def main(args):
     env = Dynamics(args.state_dim, args.action_dim)
     optimal_K = env.cal_optimal_K()
     K = np.zeros((args.action_dim, args.state_dim))
+    # K = optimal_K
     d = args.state_dim
 
     for epoch in range(args.epoch):
@@ -22,11 +23,11 @@ def main(args):
             sigma_i = np.zeros((args.state_dim, args.state_dim))
             state = env.reset()
             for j in range(args.l):
-                action = -np.dot(K + U_i, state)
+                action = -np.dot(K, state)
                 state, cost = env.step(action)
                 C_i += cost
                 sigma_i += np.dot(state, state.T)
-                print(np.linalg.norm(action), np.linalg.norm(state))
+                # print("norm of actions and states", np.linalg.norm(action), np.linalg.norm(state))
 
             c_gradient += C_i * U_i
             sigma_gradient += sigma_i
@@ -38,17 +39,17 @@ def main(args):
         if np.linalg.norm(gradient) >= 10:
             gradient *= (10 / np.linalg.norm(gradient))
         K = K - args.lr * gradient
-        print(np.linalg.norm(K), np.linalg.norm(optimal_K), np.linalg.norm(K - optimal_K) / np.linalg.norm(optimal_K))
+        print(epoch, np.linalg.norm(K), np.linalg.norm(optimal_K), np.linalg.norm(K - optimal_K) / np.linalg.norm(optimal_K))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--state_dim", default=100, type=int)
     parser.add_argument("--action_dim", default=20, type=int)
-    parser.add_argument("--l", default=100000000, type=int, help="roll-out length")
-    parser.add_argument("--m", default=1, type=int, help="number of trajectories")
+    parser.add_argument("--l", default=100, type=int, help="roll-out length")
+    parser.add_argument("--m", default=100, type=int, help="number of trajectories")
     parser.add_argument("--r", default=0.01, type=float, help="smoothing parameter")
-    parser.add_argument("--epoch", default=1, type=int, help="number of training epochs")
-    parser.add_argument("--lr", default=1e-3, type=float, help="learning rate")
+    parser.add_argument("--epoch", default=10000, type=int, help="number of training epochs")
+    parser.add_argument("--lr", default=1e-4, type=float, help="learning rate")
     args = parser.parse_args()
     main(args=args)
