0aa9dd86bf8b0c815ac204698ce93683bf008ee9
xiangyu_liu <liuxiangyu999@sjtu.edu.cn>
2020-01-24 21:28:01 -0800

    initial commit

---
+++ dynamics.py
@@ -0,0 +1,76 @@
+import numpy as np
+# np.random.seed(0)
+import warnings
+import control
+warnings.simplefilter('error', RuntimeWarning)
+
+
+class Dynamics:
+    def __init__(self, state_dim, action_dim):
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.initialize_dynamics()
+
+    def initialize_dynamics(self):
+        self.A = np.random.rand(*[self.state_dim, self.state_dim])
+        self.B = np.random.rand(*[self.state_dim, self.action_dim])
+
+        A_eig_max = max([i.real for i in np.linalg.eigvals(self.A) if i.imag == 0])
+        print(A_eig_max)
+        if A_eig_max >= 1:
+            self.A = self.A - (A_eig_max - 0.5) * np.diag([1, ] * self.state_dim)
+            # self.A = self.A / A_eig_max
+        print("max value:", max([i.real for i in np.linalg.eigvals(self.A) if i.imag == 0]))
+        self.Q = np.random.rand(*[self.state_dim, self.state_dim])
+        Q_eig_min = min(i.real for i in np.linalg.eigvals(self.Q) if i.imag == 0)
+        # print(Q_eig_min)
+        # make sure Q is definite
+        if Q_eig_min < 0:
+            self.Q = self.Q - Q_eig_min * np.diag([1, ] * self.state_dim)
+
+        self.R = np.random.rand(*[self.action_dim, self.action_dim])
+        R_eig_min = min(i.real for i in np.linalg.eigvals(self.R) if i.imag == 0)
+        # print(R_eig_min)
+        # make sure R is definite
+        if R_eig_min < 0:
+            self.R = self.R - R_eig_min * np.diag([1, ] * self.action_dim)
+
+    def reset(self):
+        self.state = np.random.rand(*[self.state_dim, 1])
+        return self.state
+
+    def step(self, action):
+        try:
+            with warnings.catch_warnings():
+                self.state = np.dot(self.A, self.state) + np.dot(self.B, action)
+        except:
+            # print(self.state)
+            pass
+        reward = np.linalg.multi_dot([self.state.T, self.Q, self.state]) + np.linalg.multi_dot(
+            [action.T, self.R, action])
+        return self.state, reward
+
+    def cal_optimal_K(self):
+        P = self.cal_approx_P()
+        return -np.linalg.multi_dot(
+            [np.linalg.inv(self.R + np.linalg.multi_dot([self.B.T, P, self.B])), self.B.T, P, self.A])
+
+    def cal_approx_P(self):
+        P_old = self.Q
+        i = 0
+        while True:
+            i += 1
+            P_new = self.Q + np.linalg.multi_dot([self.A.T, P_old, self.A]) - np.linalg.multi_dot(
+                [self.A.T, P_old, self.B, np.linalg.inv(self.R + np.linalg.multi_dot([self.B.T, P_old, self.B])),
+                 self.B.T, P_old, self.A])
+
+            ratio = np.linalg.norm(P_new - P_old) / np.linalg.norm(P_old)
+            print(ratio, np.linalg.norm(P_new - P_old), np.linalg.norm(P_old))
+            P_old = P_new
+            if ratio <= 1e-5:
+                print("iteration is:", i)
+                break
+        return P_new
+
+    def test_control(self):
+        pass
---
+++ lqr.py
@@ -0,0 +1,54 @@
+import numpy as np
+from dynamics import Dynamics
+import argparse
+
+
+# np.random.seed(0)
+
+def main(args):
+    env = Dynamics(args.state_dim, args.action_dim)
+    optimal_K = env.cal_optimal_K()
+    K = np.zeros((args.action_dim, args.state_dim))
+    d = args.state_dim
+
+    for epoch in range(args.epoch):
+        c_gradient = np.zeros(K.shape)
+        sigma_gradient = np.zeros((args.state_dim, args.state_dim))
+        for i in range(args.m):
+            U_i = np.random.rand(*K.shape)
+            U_i = (args.r / np.linalg.norm(U_i)) * U_i
+
+            C_i = 0
+            sigma_i = np.zeros((args.state_dim, args.state_dim))
+            state = env.reset()
+            for j in range(args.l):
+                action = -np.dot(K + U_i, state)
+                state, cost = env.step(action)
+                C_i += cost
+                sigma_i += np.dot(state, state.T)
+                print(np.linalg.norm(action), np.linalg.norm(state))
+
+            c_gradient += C_i * U_i
+            sigma_gradient += sigma_i
+
+        c_gradient *= (d / (args.m * args.r ** 2))
+        sigma_gradient *= 1 / args.m
+        gradient = np.dot(c_gradient, np.linalg.inv(sigma_gradient))
+        # clip gradient to stablize training
+        if np.linalg.norm(gradient) >= 10:
+            gradient *= (10 / np.linalg.norm(gradient))
+        K = K - args.lr * gradient
+        print(np.linalg.norm(K), np.linalg.norm(optimal_K), np.linalg.norm(K - optimal_K) / np.linalg.norm(optimal_K))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--state_dim", default=100, type=int)
+    parser.add_argument("--action_dim", default=20, type=int)
+    parser.add_argument("--l", default=100000000, type=int, help="roll-out length")
+    parser.add_argument("--m", default=1, type=int, help="number of trajectories")
+    parser.add_argument("--r", default=0.01, type=float, help="smoothing parameter")
+    parser.add_argument("--epoch", default=1, type=int, help="number of training epochs")
+    parser.add_argument("--lr", default=1e-3, type=float, help="learning rate")
+    args = parser.parse_args()
+    main(args=args)
