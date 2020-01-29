7c6d1d479b8bbb9f64ee499c482ef1fd8bf85395
Xiangyu Liu <42193741+xiangyu-liu@users.noreply.github.com>
2020-01-28 22:54:44 -0800

    update plot.py

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 import pickle
 
 if __name__ == '__main__':
-    path = r"C:\Users\11818\Desktop\RL\Code\PG4LQR\logs\2-2\natural is False smoothing parameter 0.05 lr 0.001\run1\summary.pkl"
+    path = ""
     data = pickle.load(open(path, mode="rb"))
     cost_ratio_ = [item[4] for item in data][0: 1000]
     cost_ratio = [sum(cost_ratio_[i: i + 50]) / 50 for i in range(len(cost_ratio_) - 50)]
